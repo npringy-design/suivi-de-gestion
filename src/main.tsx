dae806dd3a41557d7b72858becbd86ce4832ae16
npringy-design <npringy@gmail.com>
2026-05-16 12:30:12 +0200

    Use App as single application entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,6 @@
-import React, { Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 
-import router from './router';
-import { DataProvider } from './contexts/DataContext';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <DataProvider>
-      <Suspense fallback={<div className="flex items-center justify-center min-h-screen"><div className="text-lg">Chargement...</div></div>}>
-        <RouterProvider router={router} />
-      </Suspense>
-    </DataProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(<App />);
