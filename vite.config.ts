67ad0cb101a2e14bc7f6640d80beda6c2b486de2
npringy-design <npringy@gmail.com>
2026-06-07 10:26:46 +0200

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  const defineValues = { ['process.env.' + 'GEMINI_API_KEY']: JSON.stringify(env.GEMINI_API_KEY) };
+export default defineConfig(() => {
   const plugins = [
     react(), tailwindcss()
   ];
   return {
     plugins,
-    define: defineValues,
     resolve: { alias: { '@': path.resolve(__dirname, './src') } },
     build: {
       rollupOptions: {
